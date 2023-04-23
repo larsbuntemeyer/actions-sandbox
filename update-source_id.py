319b9ab746cc0fc1a5fc49d93b5ce59e79731715
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2023-04-23 17:16:08 +0200

    update python script

--- update-source_id.py
+++ update-source_id.py
@@ -4,7 +4,22 @@ print ("Number of arguments:", len(sys.argv), "arguments")
 print ("Argument List:", str(sys.argv))
 
 
+table = "CORDEX_source_id.json"
+
+
+def update_table(entry):
+    with open(table, "r") as f:
+        current = json.load(f)
+    current["source_id"].update(entry)
+    print(current)
+    with open(table, 'w') as f:
+       json.dump(current, f)
+
+def get_entries(content):
+    entries = json.loads(content) 
+
+
 if __name__ == "__main__":
     content = sys.argv[1]
-    entries = json.loads(content) 
-    print(entries)
+    entries = get_entries(content)
+    update_table(entry)
