24d492a2282f365a4f92297b3170578f4919cfa5
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2023-04-23 16:17:15 +0200

    updates

--- parse-issue-body.py
+++ parse-issue-body.py
@@ -4,14 +4,18 @@ print ("Argument List:", str(sys.argv))
 
 
 
+test_input = '### source-id\n\nREMO2023\n\n### What license do you choose?\n\nCC BY 4.0' 
 
 
 def parse_issue_body(issue):
     lines = issue.splitlines()
-    print(lines)
+    lines = [line for line in lines if line]
+    keys = lines[::2]; values = lines[1::2]
+    return {k.split()[1]: v for k,v in zip(keys, values)}    
 
 
 
 if __name__ == "__main__":
-    issue_body = sys.argv[1]
-    parse_issue_body(issue_body)
+    issue_body = test_input #sys.argv[1]
+    entries = parse_issue_body(issue_body)
+    print(entries)
