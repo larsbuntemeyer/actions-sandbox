1dcf3dd5584504a7a16bc5adc813451596dcb2ad
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2023-04-23 17:40:19 +0200

    update script

--- update-source_id.py
+++ update-source_id.py
@@ -1,17 +1,20 @@
 import sys
 import json
+import pprint
 print ("Number of arguments:", len(sys.argv), "arguments")
 print ("Argument List:", str(sys.argv))
 
 
 table = "CORDEX_source_id.json"
-
+table_name = "source_id"
 
 def update_table(entry):
     with open(table, "r") as f:
         current = json.load(f)
-    current["source_id"].update(entry)
-    print(current)
+    source_id = entry["source_id"]
+    if source_id not in current[table_name]:
+        current[table_name][source_id] = entry
+    pprint.pprint(current)
     with open(table, 'w') as f:
        json.dump(current, f)
 
@@ -21,5 +24,6 @@ def get_entries(content):
 
 if __name__ == "__main__":
     content = sys.argv[1]
-    entries = get_entries(content)
-    update_table(entries)
+    entry = get_entries(content)
+    print(entry)
+    update_table(entry)
