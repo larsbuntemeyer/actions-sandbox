5b407577d0512041c26bb7e9e4f25aaf23109fa8
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2023-04-23 11:26:14 +0200

    update script

--- parse-issue-body.py
+++ parse-issue-body.py
@@ -1,3 +1,17 @@
 import sys
 print ("Number of arguments:", len(sys.argv), "arguments")
 print ("Argument List:", str(sys.argv))
+
+
+
+
+
+def parse_issue_body(issue):
+    lines = issue.splitlines()
+    print(lines)
+
+
+
+if __name__ == "__main__":
+    issue_body = sys.argv[1]
+    parse_issue_body(issue_body)
